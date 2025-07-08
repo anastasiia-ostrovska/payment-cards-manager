bb81b574bfa7331a544c678b71f8a97bd02aefbd
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-07-08 19:49:41 +0300

    feat: create a Loading page with as a main fallback;

---
+++ src/pages/Loading.tsx
@@ -0,0 +1,12 @@
+export const Loading = () => {
+	return (
+		<main
+			className={`
+		flex flex-col items-center justify-center justify-self-center 
+		h-dvh w-full max-w-lg 
+		gap-4 p-4`}
+		>
+			<p className="font-bold text-2xl text-nowrap">Loading...</p>
+		</main>
+	);
+};
